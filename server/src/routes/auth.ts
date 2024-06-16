eecdee2b100bd9bd3e060016901aba80489d4b4e
2018007956 <bbmt1906@hanyang.ac.kr>
2024-06-16 15:56:29 +0900

    feat: set cookie options
    #11

--- server/src/routes/auth.ts
+++ server/src/routes/auth.ts
@@ -82,7 +82,11 @@ const login = async (req: Request, res: Response) => {
         const token = jwt.sign({username}, process.env.JWT_SECRET);
 
         // 쿠키 저장
-        res.set("Set-Cookie", cookie.serialize("token", token));
+        res.set("Set-Cookie", cookie.serialize("token", token, {
+            httpOnly: true,
+            maxAge: 60 * 60 * 24 * 7, // 1 week
+            path: "/"
+        }));
 
         return res.json({user, token});
     } catch (error) {
