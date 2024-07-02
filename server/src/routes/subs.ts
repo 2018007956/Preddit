5d642d591a4d54f1fe7a344b297e969e5d9f5bfe
Chaea Kim <chaea11s0@gmail.com>
2024-07-02 18:17:46 +0900

    feat: show top communities list in main page (#22)
    * feat: side bar UI
    
    #21
    
    * feat: get top subs list in db using useSWR
    
    #21
    
    * fix: code err
    troubleshooting community not generating
    #15
    
    * feat: top subs list UI
    
    #21
    
    * feat: allow domain to import an image
    
    #21
    
    * feat: show the create sub button only when authenticated
    
    #21

--- server/src/routes/subs.ts
+++ server/src/routes/subs.ts
@@ -1,11 +1,11 @@
 import { Request, Response, Router } from "express";
-import jwt from "jsonwebtoken";
 import User from "../entities/User";
 import userMiddleware from "../middlewares/user";
 import authMiddleware from "../middlewares/auth";
 import { isEmpty } from "class-validator";
-import { getRepository } from "typeorm";
 import Sub from "../entities/Sub";
+import { AppDataSource } from "../data-source";
+import Post from "../entities/Post";
 
 const createSub = async (req: Request, res: Response) => {
     const {name, title, description} = req.body;
@@ -15,10 +15,10 @@ const createSub = async (req: Request, res: Response) => {
         if(isEmpty(name)) errors.name = "이름은 비워둘 수 없습니다.";
         if(isEmpty(title)) errors.title = "제목은 비워둘 수 없습니다.";
         
-        const sub = await getRepository(Sub)    // select data in db using QueryBuilder
+        const sub = await AppDataSource.getRepository(Sub)    // select data in db using QueryBuilder
             .createQueryBuilder("sub")
             .where("lower(sub.name) = :name", { name: name.toLowerCase() })
-            .getOne()
+            .getOne();
 
         if(sub) errors.name = "서브가 이미 존재합니다.";
 
@@ -51,8 +51,28 @@ const createSub = async (req: Request, res: Response) => {
     }
 };
 
+const topSubs = async (_: Request, res: Response) => {
+    try {
+        const imageUrlExp = `COALESCE(s."imageUrn", 'https://www.gravatar.com/avatar?d=mp&f=y')`;
+        const subs = await AppDataSource
+            .createQueryBuilder()
+            .select(`s.title, s.name, ${imageUrlExp} as "imageUrl", count(p.id) as "postCount"`)
+            .from(Sub, "s")
+            .leftJoin(Post, "p", `s.name = p."subName"`)
+            .groupBy('s.title, s.name, "imageUrl"')
+            .orderBy(`"postCount"`, "DESC")
+            .limit(5)
+            .execute();
+        return res.json(subs); // 프론트로 전달
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({ error: "Something went wrong" });
+    }
+}
+
 const router = Router();
 
 router.post("/", userMiddleware, authMiddleware, createSub);   // call handler
+router.get("/sub/topSubs", topSubs);
 
 export default router;
