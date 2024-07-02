d69d1b1b060b50fd083ddfd966e419ac3f0d5a16
2018007956 <bbmt1906@hanyang.ac.kr>
2024-07-02 14:19:23 +0900

    feat: side bar UI
    #21

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,8 +1,31 @@
+import Link from "next/link"
+
 export default function Home() {
   return (
-    <main
-      className="flex min-h-screen flex-col items-center justify-between p-24"
-    >
-    </main>
+    <div className='flex max-w-5xl px-4 pt-5 mx-auto'>
+      {/* 포스트 리스트 */}
+      <div className='w-full md:mr-3 md:w-8/12'>  </div>
+
+      {/* 사이드바 */}
+      <div className='hidden w-4/12 ml-3 md:block'>
+        <div className='bg-white border rounded'>
+          <div className='p-4 border-b'>
+            <p className='text-lg font-semibold text-center'>상위 커뮤니티</p>
+          </div>
+
+          {/* 커뮤니티 리스트 */}
+          <div></div>
+          <div className='w-full py-6 text-center'>
+            <Link href="/subs/create">
+              <span className='w-full p-2 text-center text-white bg-gray-400 rounded'>
+                커뮤니티 만들기
+              </span>
+            </Link>
+          </div>
+          
+        </div>
+      </div>
+
+    </div>
   );
 }
