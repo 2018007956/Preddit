14d16aa82b11f9b9fb58a9ba2678feacf3a45c5f
2018007956 <bbmt1906@hanyang.ac.kr>
2024-07-03 15:44:48 +0900

    feat: sub info page UI
    #23

--- client/src/pages/r/[sub].tsx
+++ client/src/pages/r/[sub].tsx
@@ -1,4 +1,5 @@
 import axios from 'axios'
+import Image from 'next/image';
 import { useRouter } from 'next/router';
 import React from 'react'
 import useSWR from 'swr';
@@ -17,9 +18,58 @@ const SubPage = () => {
     const router = useRouter();
     const subName = router.query.sub;
     const { data: sub, error } = useSWR(subName ? `/subs/${subName}` : null, fetcher);
-
+    console.log('sub', sub);
     return (
-    <div>SubPage</div>
+        <>
+            {sub &&
+                <>
+                    <div>
+                        {/* 배터 이미지 */}
+                        <div className="bg-gray-400">
+                            {sub.bannerUrl ? (
+                                <div
+                                    className='h-56'
+                                    style={{
+                                        backgroundImage: `url(${sub.bannerUrl})`,
+                                        backgroundRepeat: "no-repeat",
+                                        backgroundSize: "cover",
+                                        backgroundPosition: "center",
+                                    }}
+                                ></div>
+                            ) : (
+                                <div className="h-20 bg-gray-400"></div>
+                            )}
+                        </div>
+                        {/* Sub meta data */}
+                        <div className='h-20 bg-white'>
+                            <div className='relative flex max-w-5xl px-5 mx-auto'>
+                                <div className='absolute' style={{ top: -15 }}>
+                                    {sub.imageUrl && (
+                                        <Image
+                                            src={sub.imageUrl}
+                                            alt="커뮤니티 이미지"
+                                            width={70}
+                                            height={70}
+                                            className="rounded-full"
+                                        />
+                                    )}
+                                </div>
+                                <div className='pt-1 pl-24'>
+                                    <div className='flex items-center'>
+                                        <h1 className='text-3xl font-bold'>{sub.title}</h1>
+                                    </div>
+                                    <p className='font-bold text-gray-400 text-small'>
+                                        /r/{sub.name}
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    {/* Posts & Sidebar */}
+                    <div className='flex max-w-5xl px-4 pt-5 mx-auto'></div>
+                </>
+            }
+        </>
     )
 }
 
