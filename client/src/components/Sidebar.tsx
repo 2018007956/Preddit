fd3b97e23b162dd42483a826d82b71a7181c4ba9
2018007956 <bbmt1906@hanyang.ac.kr>
2024-07-04 23:36:55 +0900

    feat: side bar component
    #26

---
+++ client/src/components/Sidebar.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Sub } from '../types'
+type Props = {
+    sub: Sub
+}
+
+const Sidebar = ({ sub }: Props) => {
+    return (
+      <div>Sidebar</div>
+    )
+  }
+  
+  export default Sidebar
