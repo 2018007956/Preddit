1406721458aad31ad802e17a97a5417c55619b0f
2018007956 <bbmt1906@hanyang.ac.kr>
2024-07-07 14:16:27 +0900

    feat: post page UI
    #30

--- client/src/pages/r/[sub]/[identifier]/slug.tsx
+++ client/src/pages/r/[sub]/[identifier]/slug.tsx
@@ -1,5 +1,7 @@
 import { Post } from '@/src/types';
 import axios from 'axios';
+import dayjs from 'dayjs';
+import Link from 'next/link';
 import { useRouter } from 'next/router';
 import useSWR from 'swr';
 
@@ -11,7 +13,46 @@ const PostPage = () => {
         identifier && slug ? `/posts/${identifier}/${slug}` : null
     )
     return (
-    <div>PostPage</div>
+        <div className="flex max-w-5xl px-4 pt-5 mx-auto">
+            {/* Post */}
+            <div className="w-full md:mr-3 md:w-8/12">
+                <div className="bg-white rounded">
+                    {post && (
+                        <>
+                            <div className="flex">
+                                <div className="py-2 pr-2">
+                                    <div className="flex items-center">
+                                        <p className="text-xs test-gray-400">
+                                            Posted by 
+                                            <Link href={`/u/${post.username}`}>
+                                                <span className="mx-1 hover:underline">
+                                                    /u/{post.username}
+                                                </span>
+                                            </Link>
+                                            <Link href={post.url}>
+                                                <span className="mx-1 hover:underline">
+                                                    {dayjs(post.createdAt).format("YYYY-MM-DD HH:mm")}
+                                                </span>
+                                            </Link>
+                                        </p>
+                                    </div>
+                                    <h1 className="my-1 text-xl font-medium">{post.title}</h1>
+                                    <p className="my-3 text-sm">{post.body}</p>
+                                    <div className="flex">
+                                        <button>
+                                            <i className="mr-1 fas fa-comment-alt fa-xs"></i>
+                                            <span className="font-bold">
+                                                {post.commentCount} Comments
+                                            </span>
+                                        </button>
+                                    </div>
+                                </div>
+                            </div>
+                        </>
+                    )}
+                </div>
+            </div>
+        </div>
     )
 }
 
