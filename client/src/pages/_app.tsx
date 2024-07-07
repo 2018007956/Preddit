4820173cf943e7e5eb9523c85173896e2f35fce5
Chaea Kim <chaea11s0@gmail.com>
2024-07-07 15:48:44 +0900

    feat: post page (#31)
    * feat: get post data using useSWR
    
    #30
    
    * feat: set fetcher at once in _app
    
    #30
    
    * feat: getPost API
    
    #30
    
    * feat: post page UI
    
    #30
    
    * fix: slug of the path to dynamic
    
    #30
    
    * feat: icon settings using font-awesome
    
    #30

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -4,6 +4,9 @@ import type { AppProps } from "next/app";
 import { AuthProvider } from '../context/auth';
 import { useRouter } from 'next/router';
 import NavBar from '../components/NavBar';
+import { SWRConfig } from 'swr';
+import axios from 'axios';
+import Head from 'next/head';
 
 export default function App({ Component, pageProps }: AppProps) {
   Axios.defaults.baseURL = process.env.NEXT_PUBLIC_SERVER_BASE_URL + "/api";
@@ -13,10 +16,24 @@ export default function App({ Component, pageProps }: AppProps) {
   const authRoutes = ["/register", "/login"];
   const authRoute = authRoutes.includes(pathname);
 
-  return <AuthProvider>
-    {!authRoute && <NavBar />}
-    <div className={authRoute ? "" : "pt-16"}>
-      <Component {...pageProps} />
-    </div>
-  </AuthProvider>
+  const fetcher = async (url: string) => {
+    try {
+        const res = await axios.get(url);
+        return res.data;
+    } catch (error: any) {
+        throw error.response.data;
+    }
+  }
+
+  return <>
+    <script defer src="https://use.fontawesome.com/releases/v6.1.1/js/all.js" integrity="sha384-xBXmu0dk1bEoiwd71wOonQLyH+VpgR1XcDH3rtxrLww5ajNTuMvBdL5SOiFZnNdp" crossOrigin="anonymous"></script>
+    <SWRConfig value={{fetcher}}>
+      <AuthProvider>
+        {!authRoute && <NavBar />}
+        <div className={authRoute ? "" : "pt-16"}>
+          <Component {...pageProps} />
+        </div>
+      </AuthProvider>
+    </SWRConfig>
+  </>
 }
