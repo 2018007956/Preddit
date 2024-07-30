02804987b9782bf1dfc79887eecd19c920404a1d
Chaea Kim <chaea11s0@gmail.com>
2024-07-30 23:33:11 +0900

    feat: post card in community info page (#37)
    * feat: post card components
    
    #36
    
    * feat: post card UI
    
    #36
    
    * feat: vote function
    
    #36
    
    * feat: real-time votes using mutate
    
    #36
    
    * feat: page branching
    
    #36
    
    * fix: post url error
    
    #36

--- client/src/pages/r/[sub].tsx
+++ client/src/pages/r/[sub].tsx
@@ -1,5 +1,7 @@
+import PostCard from '@/src/components/PostCard';
 import Sidebar from '@/src/components/Sidebar';
 import { useAuthState } from '@/src/context/auth';
+import { Post } from '@/src/types';
 import axios from 'axios'
 import Image from 'next/image';
 import { useRouter } from 'next/router';
@@ -13,7 +15,7 @@ const SubPage = () => {
     const fileInputRef = useRef<HTMLInputElement>(null);
     const router = useRouter();
     const subName = router.query.sub;
-    const { data: sub, error } = useSWR(subName ? `/subs/${subName}` : null);
+    const { data: sub, error, mutate } = useSWR(subName ? `/subs/${subName}` : null);
     console.log('sub', sub);
 
     useEffect(() => {
@@ -45,6 +47,17 @@ const SubPage = () => {
            fileInput.name = type;
            fileInput.click(); 
         }
+    }
+
+    let renderPosts;
+    if(!sub) {
+        renderPosts = <p className="text-lg text-center">로딩중...</p>;
+    } else if (sub.posts.length === 0) {
+        renderPosts = <p className='text-lg text-center'>아직 작성된 포스트가 없습니다.</p>;
+    } else {
+        renderPosts = sub.posts.map((post: Post) => (
+            <PostCard key={post.identifier} post={post} subMutate={mutate} />
+        ));
     }
 
     return (
@@ -101,7 +114,7 @@ const SubPage = () => {
                     </div>
                     {/* Posts & Sidebar */}
                     <div className='flex max-w-5xl px-4 pt-5 mx-auto'>
-                        <div className='w-full md:mr-3 md:w-8/12'></div>
+                        <div className='w-full md:mr-3 md:w-8/12'>{renderPosts}</div>
                         <Sidebar sub={sub} />
                     </div>
                 </>
