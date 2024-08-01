f4f20fba3336090b23774d1715412cf79ab349cd
Chaea Kim <chaea11s0@gmail.com>
2024-08-01 23:53:52 +0900

    feat: set production environment (#47)
    * feat: split env files into [dev/prod] using env-cmd
    
    #46
    
    * feat: pm2 config file
    
    #46
    
    * fix: error correction
    
    #46
    
    * feat: images in production
    
    #46
    
    * feat: env dev file in front (commit missed)
    
    #46
    
    * feat: split env files into [dev/prod] using env-cmd
    
    #46
    
    * feat: pm2 config file
    
    #46
    
    * fix: hard-coded address to env variable
    
    #46
    
    * fix: production url will be updated later
    
    #46

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -12,7 +12,7 @@ export default function Home() {
   const { authenticated } = useAuthState();
   const fetcher = async (url: string) =>
     await axios.get(url).then(res => res.data);
-  const address = "http://localhost:4000/api/subs/sub/topSubs";
+  const address = "/subs/sub/topSubs";
 
   const getKey = (pageIndex: number, previousPageData: Post[]) => {
     if (previousPageData && !previousPageData.length) return null
