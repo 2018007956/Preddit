536acec549c97f6c2b503ba6d5f13877526e4801
2018007956 <bbmt1906@hanyang.ac.kr>
2024-08-28 14:07:54 +0900

    fix: install missing packages

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,5 +1,4 @@
 import '../styles/globals.css'
-import Axios from "axios";
 import type { AppProps } from "next/app";
 import { AuthProvider } from '../context/auth';
 import { useRouter } from 'next/router';
@@ -9,8 +8,8 @@ import axios from 'axios';
 import Head from 'next/head';
 
 export default function App({ Component, pageProps }: AppProps) {
-  Axios.defaults.baseURL = process.env.NEXT_PUBLIC_SERVER_BASE_URL + "/api";
-  Axios.defaults.withCredentials = true;
+  axios.defaults.baseURL = process.env.NEXT_PUBLIC_SERVER_BASE_URL + "/api";
+  axios.defaults.withCredentials = true;
 
   const { pathname } = useRouter();
   const authRoutes = ["/register", "/login"];
