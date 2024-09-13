dd9ef0b457f2121cbb5d4e432521314475160e99
2018007956 <bbmt1906@hanyang.ac.kr>
2024-09-13 23:42:42 +0900

    fix: add server domain to nextconfig

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -5,7 +5,8 @@ const nextConfig = {
     domains: [
       "www.gravatar.com", 
       "localhost",
-      "preddit-on.vercel.app"
+      "preddit-on.vercel.app",
+      "ec2-13-124-135-251.ap-northeast-2.compute.amazonaws.com"
     ]
   }
 };
