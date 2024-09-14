07537b343094e79d84428f138043b6486c8b5931
2018007956 <bbmt1906@hanyang.ac.kr>
2024-09-14 15:30:56 +0900

    fix: CORS error when vercel deploy

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -8,6 +8,20 @@ const nextConfig = {
       "preddit-on.vercel.app",
       "ec2-13-124-135-251.ap-northeast-2.compute.amazonaws.com"
     ]
+  },
+  async headers() {
+    return [
+      {
+        // matching all API routes
+        source: "/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+        ]
+      }
+    ]
   }
 };
 
